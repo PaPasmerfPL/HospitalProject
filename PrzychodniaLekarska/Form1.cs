f26e3a7941d492a73a97a1a72cecbea3f6521d82
PaPaSmerfPL <waluch.k@gmail.com>
2015-09-13 00:39:33 +0200

    13.09.2015a

--- PrzychodniaLekarska/Form1.cs
+++ PrzychodniaLekarska/Form1.cs
@@ -16,33 +16,58 @@ namespace PrzychodniaLekarska
         public LinkedList<Pacjent> wszyscyPacjenci = new LinkedList<Pacjent>();
         public Semaphore dostepGrafikaSemaphore = new Semaphore(1,1);
 
-        public LinkedList<Lekarz> lekarze = new LinkedList<Lekarz>();
+        public List<Lekarz> lekarze = new List<Lekarz>();
 
         public int sleepVal = 20;
-
+        /*
         Lekarz lekarz1;
         Lekarz lekarz2;
         Lekarz lekarz3;
-        Lekarz lekarz4;
+        Lekarz lekarz4;*/
+
 
         public Form1()
         {
             InitializeComponent();
+            int LICZBA_LEKARZY = 3;
+            int k = 240;
+
+            for (int i = 0; i < LICZBA_LEKARZY; i++)
+            {
+                
+                Lekarz lekarz = new Lekarz(this, new Point(k, 180), new Point(k, 90));
+                lekarz.praca = new Thread(new ThreadStart(lekarz.Pracuj));
+                lekarze.Add(lekarz);
+                k = k + 300;
+            }
+            for (int i = 0; i < LICZBA_LEKARZY; i++)
+            {
+                Lekarz lekarz = lekarze.ElementAt(i);
+                Thread generator = new Thread(new ThreadStart(() => generatorPacjentowLekarz(lekarz)));
+                generator.Name = "lekarz" + (i + 1);
+                generator.Start();
+                System.Console.WriteLine(generator.Name);
+            }
+            foreach (Lekarz lekarz in lekarze)
+            {
+                lekarz.praca.Start();
+            }
+            /*
             //Lekarz z lewej
-            lekarz1 = new Lekarz(this , new Point(240,180  ), new Point(240, 90));
-            this.lekarze.AddLast(lekarz1);
+            lekarz1 = new Lekarz(this, new Point(240, 180), new Point(240, 90));
+            //this.lekarze.AddLast(lekarz1);
             lekarz1.praca = new Thread(new ThreadStart( lekarz1.Pracuj ));
             //Lekarz w srodku
             lekarz2 = new Lekarz(this, new Point(560, 180), new Point(560, 90));
-            this.lekarze.AddLast(lekarz2);
+            //this.lekarze.AddLast(lekarz2);
             lekarz2.praca = new Thread(new ThreadStart(lekarz2.Pracuj));
             //Lekarz z prawej
             lekarz3 = new Lekarz(this, new Point(860, 180), new Point(860, 90));
-            this.lekarze.AddLast(lekarz3);
+            //this.lekarze.AddLast(lekarz3);
             lekarz3.praca = new Thread(new ThreadStart(lekarz3.Pracuj));
             //xD
             lekarz4 = new Lekarz(this, new Point(1160, 180), new Point(1160, 90));
-            this.lekarze.AddLast(lekarz4);
+           // this.lekarze.AddLast(lekarz4);
             lekarz4.praca = new Thread(new ThreadStart(lekarz4.Pracuj));
 
             Thread generator1 = new Thread(new ThreadStart(() => generatorPacjentowLekarz(lekarz1)));
@@ -51,7 +76,6 @@ namespace PrzychodniaLekarska
             generator2.Name = "lekarz2";
             Thread generator3 = new Thread(new ThreadStart(() => generatorPacjentowLekarz(lekarz3)));
             generator3.Name = "lekarz3";
-
             Thread generator4 = new Thread(new ThreadStart(() => generatorPacjentowLekarz(lekarz4)));
             generator4.Name = "lekarz4";
 
@@ -63,7 +87,7 @@ namespace PrzychodniaLekarska
             lekarz1.praca.Start();
             lekarz2.praca.Start();
             lekarz3.praca.Start();
-            lekarz4.praca.Start();
+            lekarz4.praca.Start();*/
         }
 
         private void generatorPacjentowLekarz(Lekarz _lekarz2)
@@ -74,7 +98,7 @@ namespace PrzychodniaLekarska
                 Random rand = new Random();
                 int i = 0; //iterator testow
                 int bO = 0;
-                while (i < 4)
+                while (i < 5)
                 {
                     //TODO zmienic na losowa ilosc przypisywanych lekarzy
                     Pacjent temp = new Pacjent(_lekarz2, this);
@@ -82,36 +106,57 @@ namespace PrzychodniaLekarska
                     //Badania okresowe
 
 
-                    bO = rand.Next(1,4);
+                    bO = rand.Next(1,5);
+                    
+                    foreach (Lekarz lekarz in lekarze) {
+                        
+                        for (int j = 0; j < bO;j++)
+                        {
+                            int randIndex = rand.Next(lekarze.Count);
+                            Lekarz kandydat = lekarze.ElementAt(randIndex);
+                            if (Thread.CurrentThread.Name.Equals(lekarz.praca.Name))
+                            {
+                                j--;
+                            }
+                           
+                            if (!temp.lekarzeDoOdwiedzenia.Contains(kandydat))
+                            {
+                                temp.lekarzeDoOdwiedzenia.AddLast(kandydat);
+                                
+                            }
+                        }
+
+                    }
+                   /* 
                     if (Thread.CurrentThread.Name.Equals("lekarz1"))
                     {
-                        if (bO == 1) temp.lekarzeDoOdwiedzenia.AddLast(this.lekarz2);
+                        if (bO == 1) temp.lekarzeDoOdwiedzenia.AddLast(this.lekarze.ElementAt(1));
                         if (bO == 2)
                         {
-                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarz2);
-                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarz3);
+                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarze.ElementAt(1));
+                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarze.ElementAt(2));
                         }
                         
                     }
                     else if(Thread.CurrentThread.Name.Equals("lekarz2"))
                     {
-                        if (bO == 1) temp.lekarzeDoOdwiedzenia.AddLast(this.lekarz1);
+                        if (bO == 1) temp.lekarzeDoOdwiedzenia.AddLast(this.lekarze.ElementAt(0));
                         if (bO == 2)
                         {
-                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarz1);
-                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarz3);
+                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarze.ElementAt(0));
+                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarze.ElementAt(2));
                         }
                     }
                     else if (Thread.CurrentThread.Name.Equals("lekarz3"))
                     {
-                        if (bO == 1) temp.lekarzeDoOdwiedzenia.AddLast(this.lekarz2);
+                        if (bO == 1) temp.lekarzeDoOdwiedzenia.AddLast(this.lekarze.ElementAt(1));
                         if (bO == 2)
                         {
-                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarz1);
+                            temp.lekarzeDoOdwiedzenia.AddLast(this.lekarze.ElementAt(0));
                             
                         }
                     }
-
+                    */
                     this.dostepGrafikaSemaphore.WaitOne();
                     wszyscyPacjenci.AddLast(temp);
                     this.dostepGrafikaSemaphore.Release();
